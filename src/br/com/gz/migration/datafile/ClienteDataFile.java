0ffcc898df237dbe66c13b91a10463155317eb79
JSansalone <jsansalone@hotmail.com>
2012-06-25 23:13:17 -0300

    Sincronizando a API DataFile com esta branch

--- src/br/com/gz/migration/datafile/ClienteDataFile.java
+++ src/br/com/gz/migration/datafile/ClienteDataFile.java
@@ -2,30 +2,71 @@ package br.com.gz.migration.datafile;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
+import br.com.gz.bean.Cliente;
 import br.com.gz.migration.EnMigrationDataType;
 import br.com.gz.migration.exception.InvalidCellTypeException;
 import br.com.gz.migration.exception.InvalidMigrationDataTypeException;
 import br.com.gz.migration.exception.ReachedTheEndOfFileException;
 import br.com.gz.migration.exception.ReachedTheStartOfFileException;
 import br.com.gz.migration.exception.RequiredColumnNotFoundException;
+import br.com.gz.migration.policy.EnColumnsCategory;
+import br.com.gz.migration.policy.EnMercoFlexRequiredColumns;
+import br.com.gz.util.Formattable;
 import br.com.gz.util.GZSoftwares;
+import br.com.gz.util.MercattoFormat;
+import br.com.gz.util.MercoFlexFormat;
 
 public class ClienteDataFile extends DataFile {
 
 	private static ClienteDataFile instance;
-	
-	protected ClienteDataFile(GZSoftwares software)
-			throws InvalidMigrationDataTypeException, IOException, RequiredColumnNotFoundException {
-		super(software,EnMigrationDataType.CLIENTE);
 
-		try {
-			checkHeaderPolicy();
-		} catch (InvalidCellTypeException e) {
-			// johnny Auto-generated catch block
-			e.printStackTrace();
+	private ClienteDataFile(GZSoftwares software)
+			throws InvalidMigrationDataTypeException, IOException {
+		super(software, EnMigrationDataType.CLIENTE);
+
+		requiredColumns = EnMercoFlexRequiredColumns
+				.filterValues(EnColumnsCategory.CLIENTES);
+
+		qtyRequiredColumns = requiredColumns.length;
+
+	}
+
+	/**
+	 * Método que implementa o singleton. Garante que em toda a execução da
+	 * aplicação só exista uma instância dessa classe
+	 * 
+	 * @return - A instância da classe
+	 * @throws IOException
+	 *             - Se não conseguir ler o arquivo
+	 * @throws RequiredColumnNotFoundException
+	 */
+	public static ClienteDataFile getInstance(GZSoftwares software)
+			throws IOException {
+
+		if (instance == null) {
+
+			try {
+
+				instance = new ClienteDataFile(software);
+
+			} catch (IOException e) {
+
+				throw e;
+
+			} catch (InvalidMigrationDataTypeException e) {
+
+				// johnny Auto-generated catch block
+				e.printStackTrace();
+
+			}
+
 		}
-		
+
+		return instance;
+
 	}
 
 	@Override
@@ -36,81 +77,392 @@ public class ClienteDataFile extends DataFile {
 
 	@Override
 	public int getTotalRows() {
-		// johnny Auto-generated method stub
-		return 0;
+		
+		int i = currentIndex;
+		int j=0;
+		
+		while(hasNext()){
+			try {
+				next();
+				j++;
+			} catch (ReachedTheEndOfFileException e) {
+				break;
+			}
+		}
+		
+		currentIndex = i;
+		
+		return j;
+		
 	}
 
 	@Override
 	public Object first() {
-		// johnny Auto-generated method stub
-		return null;
+
+		currentIndex = 2;
+
+		try {
+			return previous();
+		} catch (ReachedTheStartOfFileException e) {
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 
 	@Override
 	public Object last() {
-		// johnny Auto-generated method stub
-		return null;
+		
+		currentIndex = lastIndex;
+
+		try {
+			return next();
+		} catch (ReachedTheEndOfFileException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 
 	@Override
 	public boolean hasNext() {
-		// johnny Auto-generated method stub
+
+		Object[] data = getRowData(currentIndex);
+
+		for (Object o : data) {
+			if (!o.equals(NULL_ROW) && !o.equals(CELL_VALUE_NULL))
+				return true;
+		}
+
 		return false;
+
 	}
 
 	@Override
 	public boolean hasPrevious() {
-		// johnny Auto-generated method stub
+
+		if (currentIndex - 1 < 1)
+			return false;
+
+		Object[] data = getRowData(currentIndex - 1);
+
+		for (Object o : data) {
+			if (!o.equals(NULL_ROW) && !o.equals(CELL_VALUE_NULL))
+				return true;
+		}
+
 		return false;
+
 	}
 
 	@Override
-	protected boolean hasNextAfter(int currentIndex) {
-		// johnny Auto-generated method stub
+	protected boolean hasNextAfter(int idx) {
+
+		Object[] data = getRowData(idx + 1);
+
+		for (Object o : data) {
+			if (!o.equals(NULL_ROW) && !o.equals(CELL_VALUE_NULL))
+				return true;
+		}
+
 		return false;
+
 	}
 
 	@Override
 	public Object next() throws ReachedTheEndOfFileException {
-		// johnny Auto-generated method stub
-		return null;
+
+		Object[] o;
+
+		boolean passed = true;
+
+		do {
+			if (!hasNext())
+				throw new ReachedTheEndOfFileException();
+			o = getRowData(currentIndex++);
+			if (!checkValuesPolicy(o)) {
+				passed = false;
+				if (!notInserted.containsKey(currentIndex - 1)) {
+					notInserted.put(currentIndex - 1, o);
+				}
+			} else {
+				passed = true;
+			}
+		} while (!passed);
+
+		int i = 0;
+
+		Cliente c = new Cliente();
+
+		Formattable format;
+
+		switch (software) {
+		case MERCOFLEX:
+			format = new MercoFlexFormat();
+			break;
+
+		case MERCATTO:
+			format = new MercattoFormat();
+			break;
+
+		default:
+			format = new MercoFlexFormat();
+		}
+
+		c.setCodigo(new Integer(format.toNumeric(o[i++].toString(), false)));
+		c.setNomeFantasia(format.toNomeFantasia(o[i++].toString()));
+		c.setRazaoSocial(format.toRazaoSocial(o[i++].toString()));
+		c.setCgc(format.toNumeric(o[i++].toString(), false));
+		c.setRegistroEstadual(format.toInscricaoEstadual(o[i++].toString()));
+		c.setSexo(o[i++].toString());
+		
+		Calendar cldn = Calendar.getInstance();
+		Date dtdn = new Date(o[i++].toString());
+		cldn.setTime(dtdn);
+		c.setDataNascimento(cldn);
+		
+		Calendar cldc = Calendar.getInstance();
+		Date dtdc = new Date(o[i++].toString());
+		cldc.setTime(dtdc);
+		c.setDataCadastro(cldc);
+		
+		c.setEstado(format.toEstadoSigla(o[i++].toString()));
+		c.setCidade(o[i++].toString());
+		c.setBairro(format.toBairro(o[i++].toString()));
+		c.setEndereco(format.toEndereco(o[i++].toString()));
+		c.setCep(format.toNumeric(o[i++].toString(), false));
+		c.setNumero(new Integer(format.toNumeric(o[i++].toString(), false)));
+		c.setTelefoneResidencial(format.toNumeric(o[i++].toString(), false));
+		c.setEstadoCobranca(format.toEstadoSigla(o[i++].toString()));
+		c.setCidadeCobranca(o[i++].toString());
+		c.setBairroCobranca(format.toBairro(o[i++].toString()));
+		c.setEnderecoCobranca(format.toEndereco(o[i++].toString()));
+		c.setCepCobranca(format.toNumeric(o[i++].toString(), false));
+		c.setNumeroCobranca(new Integer(format.toNumeric(o[i++].toString(), false)));
+		c.setTelefoneCobranca(format.toNumeric(o[i++].toString(), false));
+		c.setEstadoEntrega(format.toEstadoSigla(o[i++].toString()));
+		c.setCidadeEntrega(o[i++].toString());
+		c.setBairroEntrega(format.toBairro(o[i++].toString()));
+		c.setEnderecoEntrega(format.toEndereco(o[i++].toString()));
+		c.setCepEntrega(format.toNumeric(o[i++].toString(), false));
+		c.setNumeroEnderecoEntrega(new Integer(format.toNumeric(o[i++].toString(), false)));
+		c.setTelEntrega(format.toNumeric(o[i++].toString(), false));
+		c.setSituacao(format.toSituacao(o[i++].toString()));
+		
+		return c;
+
 	}
 
 	@Override
 	public Object previous() throws ReachedTheStartOfFileException {
-		// johnny Auto-generated method stub
-		return null;
+
+		Object[] o;
+
+		boolean passed = true;
+
+		do {
+			if (!hasPrevious())
+				throw new ReachedTheStartOfFileException();
+			o = getRowData(--currentIndex);
+			if (!checkValuesPolicy(o)) {
+				passed = false;
+				if (!notInserted.containsKey(currentIndex)) {
+					notInserted.put(currentIndex, o);
+				}
+			} else {
+				passed = true;
+			}
+		} while (!passed);
+
+		int i = 0;
+
+		Cliente c = new Cliente();
+
+		Formattable format;
+
+		switch (software) {
+		case MERCOFLEX:
+			format = new MercoFlexFormat();
+			break;
+
+		case MERCATTO:
+			format = new MercattoFormat();
+			break;
+
+		default:
+			format = new MercoFlexFormat();
+		}
+
+		c.setCodigo(new Integer(format.toNumeric(o[i++].toString(), false)));
+		c.setNomeFantasia(format.toNomeFantasia(o[i++].toString()));
+		c.setRazaoSocial(format.toRazaoSocial(o[i++].toString()));
+		c.setCgc(format.toNumeric(o[i++].toString(), false));
+		c.setRegistroEstadual(format.toInscricaoEstadual(o[i++].toString()));
+		c.setSexo(o[i++].toString());
+		
+		Calendar cldn = Calendar.getInstance();
+		Date dtdn = new Date(o[i++].toString());
+		cldn.setTime(dtdn);
+		c.setDataNascimento(cldn);
+		
+		Calendar cldc = Calendar.getInstance();
+		Date dtdc = new Date(o[i++].toString());
+		cldc.setTime(dtdc);
+		c.setDataCadastro(cldc);
+		
+		c.setEstado(format.toEstadoSigla(o[i++].toString()));
+		c.setCidade(o[i++].toString());
+		c.setBairro(format.toBairro(o[i++].toString()));
+		c.setEndereco(format.toEndereco(o[i++].toString()));
+		c.setCep(format.toNumeric(o[i++].toString(), false));
+		c.setNumero(new Integer(format.toNumeric(o[i++].toString(), false)));
+		c.setTelefoneResidencial(format.toNumeric(o[i++].toString(), false));
+		c.setEstadoCobranca(format.toEstadoSigla(o[i++].toString()));
+		c.setCidadeCobranca(o[i++].toString());
+		c.setBairroCobranca(format.toBairro(o[i++].toString()));
+		c.setEnderecoCobranca(format.toEndereco(o[i++].toString()));
+		c.setCepCobranca(format.toNumeric(o[i++].toString(), false));
+		c.setNumeroCobranca(new Integer(format.toNumeric(o[i++].toString(), false)));
+		c.setTelefoneCobranca(format.toNumeric(o[i++].toString(), false));
+		c.setEstadoEntrega(format.toEstadoSigla(o[i++].toString()));
+		c.setCidadeEntrega(o[i++].toString());
+		c.setBairroEntrega(format.toBairro(o[i++].toString()));
+		c.setEnderecoEntrega(format.toEndereco(o[i++].toString()));
+		c.setCepEntrega(format.toNumeric(o[i++].toString(), false));
+		c.setNumeroEnderecoEntrega(new Integer(format.toNumeric(o[i++].toString(), false)));
+		c.setTelEntrega(format.toNumeric(o[i++].toString(), false));
+		c.setSituacao(format.toSituacao(o[i++].toString()));
+		
+		return c;
+		
 	}
 
 	@Override
 	public ArrayList<Object> getAll() {
-		// johnny Auto-generated method stub
-		return null;
+		
+		int aux = currentIndex;
+
+		currentIndex = 1;
+
+		ArrayList<Object> arD = new ArrayList<Object>();
+
+		Cliente d;
+
+		try {
+
+			while (hasNext()) {
+
+				d = (Cliente) next();
+				arD.add(d);
+
+			}
+
+		} catch (ReachedTheEndOfFileException e) {
+			System.err.println(e.getMessage());
+		}
+
+		currentIndex = aux;
+		
+		return arD;
+		
 	}
 
 	@Override
 	protected Object[] getRowData(int rowIndex) {
-		// johnny Auto-generated method stub
-		return null;
+		
+		return DataFileReader.getCellValues(dataSheet.getRow(rowIndex),
+				qtyRequiredColumns);
+		
 	}
 
 	@Override
 	public boolean checkHeaderPolicy() throws InvalidCellTypeException,
 			RequiredColumnNotFoundException {
-		// johnny Auto-generated method stub
-		return false;
+
+		// obtendo as colunas do header
+		String[] columnsFromFile = DataFileReader.getHeader(
+				dataSheet.getRow(0), qtyRequiredColumns);
+
+		ArrayList<String> notFound = new ArrayList<String>();
+		ArrayList<String> invalidType = new ArrayList<String>();
+		
+		this.notFound.clear();
+
+		// verificando se estão na ordem correta
+		for (int i = 0; i < qtyRequiredColumns; i++) {
+
+			if (requiredColumns[i].getLabel().equals(columnsFromFile[i])) {
+
+			} else {
+
+				if (columnsFromFile[i].equals(DataFile.INVALID_CELL_TYPE)) {
+
+					invalidType.add(requiredColumns[i].getLabel());
+
+				} else {
+
+					notFound.add(requiredColumns[i].getLabel());
+					
+					this.notFound.add(requiredColumns[i].getLabel());
+
+				}
+
+			}
+
+		}
+
+		if (!notFound.isEmpty()) {
+
+			String message = "As seguintes colunas não foram encontradas: ";
+			for (String s : notFound) {
+				message += s + ", ";
+			}
+			message = message.substring(0, message.length() - 2) + ".";
+			throw new RequiredColumnNotFoundException(message);
+
+		} else {
+
+			if (!invalidType.isEmpty()) {
+
+				String message = "Os tipos de dados das seguintes colunas são inválidos: ";
+				for (String s : invalidType) {
+					message += s + ", ";
+				}
+				message = message.substring(0, message.length() - 2) + ".";
+				throw new InvalidCellTypeException(message);
+
+			}
+
+		}
+
+		return true;
+
 	}
 
 	@Override
 	public boolean checkValuesPolicy(Object[] values) {
-		// johnny Auto-generated method stub
-		return false;
+
+		for (Object v : values) {
+
+			try {
+				if (v.equals(DataFile.CELL_VALUE_NULL)
+						|| v.equals(DataFile.INVALID_CELL_TYPE)
+						|| v.equals(DataFile.NULL_ROW)) {
+					return false;
+				}
+			} catch (Exception e) {
+				return false;
+			}
+
+		}
+
+		return true;
+
 	}
 
 	@Override
 	public ArrayList<String> getColumnsNotFound() {
-		// johnny Auto-generated method stub
-		return null;
+		return this.notFound;
 	}
 
 }
--- src/br/com/gz/migration/datafile/GrupoDataFile.java
+++ src/br/com/gz/migration/datafile/GrupoDataFile.java
@@ -3,29 +3,68 @@ package br.com.gz.migration.datafile;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import br.com.gz.bean.Grupo;
 import br.com.gz.migration.EnMigrationDataType;
 import br.com.gz.migration.exception.InvalidCellTypeException;
 import br.com.gz.migration.exception.InvalidMigrationDataTypeException;
 import br.com.gz.migration.exception.ReachedTheEndOfFileException;
 import br.com.gz.migration.exception.ReachedTheStartOfFileException;
 import br.com.gz.migration.exception.RequiredColumnNotFoundException;
+import br.com.gz.migration.policy.EnColumnsCategory;
+import br.com.gz.migration.policy.EnMercoFlexRequiredColumns;
+import br.com.gz.util.Formattable;
 import br.com.gz.util.GZSoftwares;
+import br.com.gz.util.MercattoFormat;
+import br.com.gz.util.MercoFlexFormat;
 
 public class GrupoDataFile extends DataFile {
 
 	private static GrupoDataFile instance;
-	
-	protected GrupoDataFile(GZSoftwares software)
-			throws InvalidMigrationDataTypeException, IOException, RequiredColumnNotFoundException {
-		super(software,EnMigrationDataType.GRUPO);
 
-		try {
-			checkHeaderPolicy();
-		} catch (InvalidCellTypeException e) {
-			// johnny Auto-generated catch block
-			e.printStackTrace();
+	private GrupoDataFile(GZSoftwares software)
+			throws InvalidMigrationDataTypeException, IOException {
+		super(software, EnMigrationDataType.GRUPO);
+
+		requiredColumns = EnMercoFlexRequiredColumns
+				.filterValues(EnColumnsCategory.GRUPO);
+
+		qtyRequiredColumns = requiredColumns.length;
+
+	}
+
+	/**
+	 * Método que implementa o singleton. Garante que em toda a execução da
+	 * aplicação só exista uma instância dessa classe
+	 * 
+	 * @return - A instância da classe
+	 * @throws IOException
+	 *             - Se não conseguir ler o arquivo
+	 * @throws RequiredColumnNotFoundException
+	 */
+	public static GrupoDataFile getInstance(GZSoftwares software)
+			throws IOException {
+
+		if (instance == null) {
+
+			try {
+
+				instance = new GrupoDataFile(software);
+
+			} catch (IOException e) {
+
+				throw e;
+
+			} catch (InvalidMigrationDataTypeException e) {
+
+				// johnny Auto-generated catch block
+				e.printStackTrace();
+
+			}
+
 		}
-		
+
+		return instance;
+
 	}
 
 	@Override
@@ -36,81 +75,318 @@ public class GrupoDataFile extends DataFile {
 
 	@Override
 	public int getTotalRows() {
-		// johnny Auto-generated method stub
-		return 0;
+		
+		int i = currentIndex;
+		int j=0;
+		
+		while(hasNext()){
+			try {
+				next();
+				j++;
+			} catch (ReachedTheEndOfFileException e) {
+				break;
+			}
+		}
+		
+		currentIndex = i;
+		
+		return j;
+		
 	}
 
 	@Override
 	public Object first() {
-		// johnny Auto-generated method stub
-		return null;
+
+		currentIndex = 2;
+
+		try {
+			return previous();
+		} catch (ReachedTheStartOfFileException e) {
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 
 	@Override
 	public Object last() {
-		// johnny Auto-generated method stub
-		return null;
+		
+		currentIndex = lastIndex;
+
+		try {
+			return next();
+		} catch (ReachedTheEndOfFileException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 
 	@Override
 	public boolean hasNext() {
-		// johnny Auto-generated method stub
+
+		Object[] data = getRowData(currentIndex);
+
+		for (Object o : data) {
+			if (!o.equals(NULL_ROW) && !o.equals(CELL_VALUE_NULL))
+				return true;
+		}
+
 		return false;
+
 	}
 
 	@Override
 	public boolean hasPrevious() {
-		// johnny Auto-generated method stub
+
+		if (currentIndex - 1 < 1)
+			return false;
+
+		Object[] data = getRowData(currentIndex - 1);
+
+		for (Object o : data) {
+			if (!o.equals(NULL_ROW) && !o.equals(CELL_VALUE_NULL))
+				return true;
+		}
+
 		return false;
+
 	}
 
 	@Override
-	protected boolean hasNextAfter(int currentIndex) {
-		// johnny Auto-generated method stub
+	protected boolean hasNextAfter(int idx) {
+
+		Object[] data = getRowData(idx + 1);
+
+		for (Object o : data) {
+			if (!o.equals(NULL_ROW) && !o.equals(CELL_VALUE_NULL))
+				return true;
+		}
+
 		return false;
+
 	}
 
 	@Override
 	public Object next() throws ReachedTheEndOfFileException {
-		// johnny Auto-generated method stub
-		return null;
+
+		Object[] o;
+
+		boolean passed = true;
+
+		do {
+			if (!hasNext())
+				throw new ReachedTheEndOfFileException();
+			o = getRowData(currentIndex++);
+			if (!checkValuesPolicy(o)) {
+				passed = false;
+				if (!notInserted.containsKey(currentIndex - 1)) {
+					notInserted.put(currentIndex - 1, o);
+				}
+			} else {
+				passed = true;
+			}
+		} while (!passed);
+
+		int i = 0;
+
+		Grupo d = new Grupo();
+
+		Formattable format;
+
+		switch (software) {
+		case MERCOFLEX:
+			format = new MercoFlexFormat();
+			break;
+
+		case MERCATTO:
+			format = new MercattoFormat();
+			break;
+
+		default:
+			format = new MercoFlexFormat();
+		}
+
+		d.setCodigo(new Integer(format.toNumeric(o[i++].toString(), false)));
+		d.setDescricao(format.toDescricaoGeral(o[i++].toString(), 20));
+		
+		return d;
+
 	}
 
 	@Override
 	public Object previous() throws ReachedTheStartOfFileException {
-		// johnny Auto-generated method stub
-		return null;
+
+		Object[] o;
+
+		boolean passed = true;
+
+		do {
+			if (!hasPrevious())
+				throw new ReachedTheStartOfFileException();
+			o = getRowData(--currentIndex);
+			if (!checkValuesPolicy(o)) {
+				passed = false;
+				if (!notInserted.containsKey(currentIndex)) {
+					notInserted.put(currentIndex, o);
+				}
+			} else {
+				passed = true;
+			}
+		} while (!passed);
+
+		int i = 0;
+
+		Grupo d = new Grupo();
+
+		Formattable format;
+
+		switch (software) {
+		case MERCOFLEX:
+			format = new MercoFlexFormat();
+			break;
+
+		case MERCATTO:
+			format = new MercattoFormat();
+			break;
+
+		default:
+			format = new MercoFlexFormat();
+		}
+
+		d.setCodigo(new Integer(format.toNumeric(o[i++].toString(), false)));
+		d.setDescricao(format.toDescricaoGeral(o[i++].toString(), 20));
+		
+		return d;
+		
 	}
 
 	@Override
 	public ArrayList<Object> getAll() {
-		// johnny Auto-generated method stub
-		return null;
+		
+		int aux = currentIndex;
+
+		currentIndex = 1;
+
+		ArrayList<Object> arD = new ArrayList<Object>();
+
+		Grupo d;
+
+		try {
+
+			while (hasNext()) {
+
+				d = (Grupo) next();
+				arD.add(d);
+
+			}
+
+		} catch (ReachedTheEndOfFileException e) {
+			System.err.println(e.getMessage());
+		}
+
+		currentIndex = aux;
+		
+		return arD;
+		
 	}
 
 	@Override
 	protected Object[] getRowData(int rowIndex) {
-		// johnny Auto-generated method stub
-		return null;
+		
+		return DataFileReader.getCellValues(dataSheet.getRow(rowIndex),
+				qtyRequiredColumns);
+		
 	}
 
 	@Override
 	public boolean checkHeaderPolicy() throws InvalidCellTypeException,
 			RequiredColumnNotFoundException {
-		// johnny Auto-generated method stub
-		return false;
+
+		// obtendo as colunas do header
+		String[] columnsFromFile = DataFileReader.getHeader(
+				dataSheet.getRow(0), qtyRequiredColumns);
+
+		ArrayList<String> notFound = new ArrayList<String>();
+		ArrayList<String> invalidType = new ArrayList<String>();
+		
+		this.notFound.clear();
+
+		// verificando se estão na ordem correta
+		for (int i = 0; i < qtyRequiredColumns; i++) {
+
+			if (requiredColumns[i].getLabel().equals(columnsFromFile[i])) {
+
+			} else {
+
+				if (columnsFromFile[i].equals(DataFile.INVALID_CELL_TYPE)) {
+
+					invalidType.add(requiredColumns[i].getLabel());
+
+				} else {
+
+					notFound.add(requiredColumns[i].getLabel());
+					
+					this.notFound.add(requiredColumns[i].getLabel());
+
+				}
+
+			}
+
+		}
+
+		if (!notFound.isEmpty()) {
+
+			String message = "As seguintes colunas não foram encontradas: ";
+			for (String s : notFound) {
+				message += s + ", ";
+			}
+			message = message.substring(0, message.length() - 2) + ".";
+			throw new RequiredColumnNotFoundException(message);
+
+		} else {
+
+			if (!invalidType.isEmpty()) {
+
+				String message = "Os tipos de dados das seguintes colunas são inválidos: ";
+				for (String s : invalidType) {
+					message += s + ", ";
+				}
+				message = message.substring(0, message.length() - 2) + ".";
+				throw new InvalidCellTypeException(message);
+
+			}
+
+		}
+
+		return true;
+
 	}
 
 	@Override
 	public boolean checkValuesPolicy(Object[] values) {
-		// johnny Auto-generated method stub
-		return false;
+
+		for (Object v : values) {
+
+			try {
+				if (v.equals(DataFile.CELL_VALUE_NULL)
+						|| v.equals(DataFile.INVALID_CELL_TYPE)
+						|| v.equals(DataFile.NULL_ROW)) {
+					return false;
+				}
+			} catch (Exception e) {
+				return false;
+			}
+
+		}
+
+		return true;
+
 	}
 
 	@Override
 	public ArrayList<String> getColumnsNotFound() {
-		// johnny Auto-generated method stub
-		return null;
+		return this.notFound;
 	}
 
 }
